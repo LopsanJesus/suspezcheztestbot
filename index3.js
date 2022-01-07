2e920381bc701eb0d2f4d3d0c15a80cce686aa6a
Jesus L <lopsanjesus@gmail.com>
2022-01-07 23:55:54 +0100

    WIzardScene

---
+++ index3.js
@@ -0,0 +1,79 @@
+import { Telegraf } from 'telegraf'
+import { Scenes } from 'telegraf'
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+const contactDataWizard = new Scenes.WizardScene(
+  'CONTACT_DATA_WIZARD_SCENE_ID', // first argument is Scene_ID, same as for BaseScene
+  (ctx) => {
+    ctx.reply('What is your name?');
+    // ctx.wizard.state.contactData = {};
+    return ctx.wizard.next();
+  },
+  (ctx) => {
+    // validation example
+    if (ctx.message.text.length < 2) {
+      ctx.reply('Please enter name for real');
+      return; 
+    }
+    // ctx.wizard.state.contactData.fio = ctx.message.text;
+    ctx.reply('Enter your e-mail');
+    return ctx.wizard.next();
+  },
+  async (ctx) => {
+    // ctx.wizard.state.contactData.email = ctx.message.text;
+    ctx.reply('Thank you for your replies, well contact your soon');
+    // await mySendContactDataMomentBeforeErase(ctx.wizard.state.contactData);
+    return ctx.scene.leave();
+  },
+);
+
+bot.command('addsong', (ctx) => {
+  await ctx.scene.enter('CONTACT_DATA_WIZARD_SCENE_ID');
+  // console.log(ctx.from)
+  // ctx.reply("¿Link de Spotify?")
+  // ctx.reply(`Tu id es ${ctx.from.id}`)
+  
+  // bot.on('text', (ctx) => {
+  //   // Comprobar que es LINK de Spotify
+  //   ctx.reply(`Vale. Link añadido. ${ctx.message.text}`)
+  // })
+
+})
+
+bot.hears('culo', (ctx) => {
+  console.log(ctx.state);
+  ctx.reply("Eso se me dise!!??!?!")
+})
+
+// bot.on('text', (ctx) => {
+//   // Explicit usage
+//   // ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`)
+
+//   // Using context shortcut
+//   ctx.reply(`Hello ${ctx.state.role}`)
+// })
+
+
+// bot.on('callback_query', (ctx) => {
+//   // Explicit usage
+//   // ctx.telegram.answerCbQuery(ctx.callbackQuery.id)
+
+//   // Using context shortcut
+//   ctx.answerCbQuery()
+// })
+
+// bot.on('inline_query', (ctx) => {
+//   const result = []
+//   // Explicit usage
+//   // ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result)
+
+//   // Using context shortcut
+//   ctx.answerInlineQuery(result)
+// })
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
--- src/index.js
+++ src/index.js
@@ -1,77 +1,55 @@
-import { Telegraf } from 'telegraf'
-import { Scenes } from 'telegraf'
+import { Composer, Markup, Scenes, Telegraf } from 'telegraf'
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const token = process.env.BOT_TOKEN
+if (token === undefined) {
+  throw new Error('BOT_TOKEN must be provided!')
+}
 
-const contactDataWizard = new Scenes.WizardScene(
-  'CONTACT_DATA_WIZARD_SCENE_ID', // first argument is Scene_ID, same as for BaseScene
-  (ctx) => {
-    ctx.reply('What is your name?');
-    // ctx.wizard.state.contactData = {};
-    return ctx.wizard.next();
+const stepHandler = new Composer()
+stepHandler.action('next', async (ctx) => {
+  await ctx.reply('Step 2. Via inline button')
+  return ctx.wizard.next()
+})
+stepHandler.command('next', async (ctx) => {
+  await ctx.reply('Step 2. Via command')
+  return ctx.wizard.next()
+})
+stepHandler.use((ctx) =>
+  ctx.replyWithMarkdown('Press `Next` button or type /next')
+)
+const superWizard = new Scenes.WizardScene(
+  'super-wizard',
+  async (ctx) => {
+    await ctx.reply(
+      'Step 1',
+      Markup.inlineKeyboard([
+        Markup.button.url('❤️', 'http://telegraf.js.org'),
+        Markup.button.callback('➡️ Next', 'next'),
+      ])
+    )
+    return ctx.wizard.next()
   },
-  (ctx) => {
-    // validation example
-    if (ctx.message.text.length < 2) {
-      ctx.reply('Please enter name for real');
-      return; 
-    }
-    // ctx.wizard.state.contactData.fio = ctx.message.text;
-    ctx.reply('Enter your e-mail');
-    return ctx.wizard.next();
+  stepHandler,
+  async (ctx) => {
+    await ctx.reply('Step 3')
+    return ctx.wizard.next()
   },
   async (ctx) => {
-    // ctx.wizard.state.contactData.email = ctx.message.text;
-    ctx.reply('Thank you for your replies, well contact your soon');
-    // await mySendContactDataMomentBeforeErase(ctx.wizard.state.contactData);
-    return ctx.scene.leave();
+    await ctx.reply('Step 4')
+    return ctx.wizard.next()
   },
-);
+  async (ctx) => {
+    await ctx.reply('Done')
+    return await ctx.scene.leave()
+  }
+)
 
-bot.command('addsong', (ctx) => {
-  await ctx.scene.enter('CONTACT_DATA_WIZARD_SCENE_ID');
-  // console.log(ctx.from)
-  // ctx.reply("¿Link de Spotify?")
-  // ctx.reply(`Tu id es ${ctx.from.id}`)
-  
-  // bot.on('text', (ctx) => {
-  //   // Comprobar que es LINK de Spotify
-  //   ctx.reply(`Vale. Link añadido. ${ctx.message.text}`)
-  // })
-
+const bot = new Telegraf(token)
+const stage = new Scenes.Stage([superWizard], {
+  default: 'super-wizard',
 })
-
-bot.hears('culo', (ctx) => {
-  console.log(ctx.state);
-  ctx.reply("Eso se me dise!!??!?!")
-})
-
-// bot.on('text', (ctx) => {
-//   // Explicit usage
-//   // ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`)
-
-//   // Using context shortcut
-//   ctx.reply(`Hello ${ctx.state.role}`)
-// })
-
-
-// bot.on('callback_query', (ctx) => {
-//   // Explicit usage
-//   // ctx.telegram.answerCbQuery(ctx.callbackQuery.id)
-
-//   // Using context shortcut
-//   ctx.answerCbQuery()
-// })
-
-// bot.on('inline_query', (ctx) => {
-//   const result = []
-//   // Explicit usage
-//   // ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result)
-
-//   // Using context shortcut
-//   ctx.answerInlineQuery(result)
-// })
-
+// bot.use(session())
+bot.use(stage.middleware())
 bot.launch()
 
 // Enable graceful stop
