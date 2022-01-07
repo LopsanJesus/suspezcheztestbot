4db883196eeff419c4d6a802199daa64d33630bf
Jesus L <lopsanjesus@gmail.com>
2022-01-07 20:34:45 +0100

    Test middleware

--- src/index.js
+++ src/index.js
@@ -2,12 +2,19 @@ import { Telegraf } from 'telegraf'
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
+bot.use(async (ctx, next) => {
+  const start = new Date()
+  await next()
+  const ms = new Date() - start
+  console.log('Response time: %sms', ms)
+})
+
 bot.command('addsong', (ctx) => {
+  console.log(ctx.from)
   ctx.reply("¿Link de Spotify?")
   
   bot.on('text', (ctx) => {
     // Comprobar que es LINK de Spotify
-    console.log(ctx.from)
     ctx.reply(`Vale. Link añadido. ${ctx.message.text}`)
   })
 
@@ -18,31 +25,31 @@ bot.hears('culo', (ctx) => {
   ctx.reply("Eso se me dise!!??!?!")
 })
 
-bot.on('text', (ctx) => {
-  // Explicit usage
-  // ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`)
+// bot.on('text', (ctx) => {
+//   // Explicit usage
+//   // ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`)
 
-  // Using context shortcut
-  ctx.reply(`Hello ${ctx.state.role}`)
-})
+//   // Using context shortcut
+//   ctx.reply(`Hello ${ctx.state.role}`)
+// })
 
 
-bot.on('callback_query', (ctx) => {
-  // Explicit usage
-  // ctx.telegram.answerCbQuery(ctx.callbackQuery.id)
+// bot.on('callback_query', (ctx) => {
+//   // Explicit usage
+//   // ctx.telegram.answerCbQuery(ctx.callbackQuery.id)
 
-  // Using context shortcut
-  ctx.answerCbQuery()
-})
+//   // Using context shortcut
+//   ctx.answerCbQuery()
+// })
 
-bot.on('inline_query', (ctx) => {
-  const result = []
-  // Explicit usage
-  // ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result)
+// bot.on('inline_query', (ctx) => {
+//   const result = []
+//   // Explicit usage
+//   // ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result)
 
-  // Using context shortcut
-  ctx.answerInlineQuery(result)
-})
+//   // Using context shortcut
+//   ctx.answerInlineQuery(result)
+// })
 
 bot.launch()
 
