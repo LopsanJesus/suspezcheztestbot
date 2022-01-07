6bd3ff0e81f60879d6196f745ce8b5870f30baa7
Jesus L <lopsanjesus@gmail.com>
2022-01-07 23:23:46 +0100

    Scenes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,39 @@
 import { Telegraf } from 'telegraf'
+import { Scenes } from 'telegraf'
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
+const scenarioTypeScene = new Scenes.BaseScene('SCENARIO_TYPE_SCENE_ID');
+
+const contactDataWizard = new Scenes.WizardScene(
+  'CONTACT_DATA_WIZARD_SCENE_ID', // first argument is Scene_ID, same as for BaseScene
+  (ctx) => {
+    ctx.reply('What is your name?');
+    ctx.wizard.state.contactData = {};
+    return ctx.wizard.next();
+  },
+  (ctx) => {
+    // validation example
+    if (ctx.message.text.length < 2) {
+      ctx.reply('Please enter name for real');
+      return; 
+    }
+    ctx.wizard.state.contactData.fio = ctx.message.text;
+    ctx.reply('Enter your e-mail');
+    return ctx.wizard.next();
+  },
+  async (ctx) => {
+    ctx.wizard.state.contactData.email = ctx.message.text;
+    ctx.reply('Thank you for your replies, well contact your soon');
+    await mySendContactDataMomentBeforeErase(ctx.wizard.state.contactData);
+    return ctx.scene.leave();
+  },
+);
 
 bot.command('addsong', (ctx) => {
-  console.log(ctx.from)
-  ctx.reply("¿Link de Spotify?")
-  ctx.reply(`${ctx.from.username}`)
+  ctx.scene.enter('CONTACT_DATA_WIZARD_SCENE_ID');
+  // console.log(ctx.from)
+  // ctx.reply("¿Link de Spotify?")
+  // ctx.reply(`Tu id es ${ctx.from.id}`)
   
   // bot.on('text', (ctx) => {
   //   // Comprobar que es LINK de Spotify
