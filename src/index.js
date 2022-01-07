a5e89d0d7d865677f59eab2402a36bc43bd4f7cb
Francisco Lopez <francisco.lopsan@gmail.com>
2022-01-08 00:53:03 +0100

    Change command

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const TelegramBot = require("node-telegram-bot-api");
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
 
 // Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
+bot.onText(/\/addsong (.+)/, (msg, match) => {
   // 'msg' is the received Message from Telegram
   // 'match' is the result of executing the regexp above on the text content
   // of the message
