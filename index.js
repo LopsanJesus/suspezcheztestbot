eb578cd5103b76343df4287f9d3e4c3a7a0f1b81
Jesus L <lopsanjesus@gmail.com>
2022-01-07 19:08:12 +0100

    Telegraf

--- index.js
+++ index.js
@@ -1,52 +1,42 @@
-var express = require("express")
-var app = express()
-var bodyParser = require("body-parser")
-const axios = require("axios")
+import { Telegraf } from 'telegraf'
 
-app.use(bodyParser.json()) // for parsing application/json
-app.use(
-	bodyParser.urlencoded({
-		extended: true,
-	})
-) // for parsing application/x-www-form-urlencoded
+const bot = new Telegraf(process.env.BOT_TOKEN)
 
-//This is the route the API will call
-app.post("/new-message", function(req, res) {
-	const { message } = req.body
+bot.command('quit', (ctx) => {
+  // Explicit usage
+  ctx.telegram.leaveChat(ctx.message.chat.id)
 
-	//Each message contains "text" and a "chat" object, which has an "id" which is the chat id
-
-	if (!message || message.text.toLowerCase().indexOf("marco") < 0) {
-		// In case a message is not present, or if our message does not have the word marco in it, do nothing and return an empty response
-		return res.end()
-	}
-
-	// If we've gotten this far, it means that we have received a message containing the word "marco".
-	// Respond by hitting the telegram bot API and responding to the appropriate chat_id with the word "Polo!!"
-	// Remember to use your own API toked instead of the one below  "https://api.telegram.org/bot<your_api_token>/sendMessage"
-	axios
-		.post(
-			"https://api.telegram.org/bot5095927198:AAGd4hqkt3IiJH8BEtRSnt2uJXEMzevIuQA/sendMessage",
-			{
-				chat_id: message.chat.id,
-				text: "Polo!!",
-			}
-		)
-		.then((response) => {
-			// We get here if the message was successfully posted
-			console.log("Message posted")
-			res.end("ok")
-		})
-		.catch((err) => {
-			// ...and here if it was not
-			console.log("Error :", err)
-			res.end("Error :" + err)
-		})
+  // Using context shortcut
+  ctx.leaveChat()
 })
 
-const PORT = process.env.PORT || 3000;
+bot.on('text', (ctx) => {
+  // Explicit usage
+  ctx.telegram.sendMessage(ctx.message.chat.id, `Hello ${ctx.state.role}`)
 
-// Finally, start our server
-app.listen(PORT, function() {
-	console.log("Telegram app listening on port 3000!")
+  // Using context shortcut
+  ctx.reply(`Hello ${ctx.state.role}`)
 })
+
+bot.on('callback_query', (ctx) => {
+  // Explicit usage
+  ctx.telegram.answerCbQuery(ctx.callbackQuery.id)
+
+  // Using context shortcut
+  ctx.answerCbQuery()
+})
+
+bot.on('inline_query', (ctx) => {
+  const result = []
+  // Explicit usage
+  ctx.telegram.answerInlineQuery(ctx.inlineQuery.id, result)
+
+  // Using context shortcut
+  ctx.answerInlineQuery(result)
+})
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
